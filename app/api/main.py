3edb065ea84104f86ddf57f9ccbbbbe6edbfe949
enk-it <gladysh.danil@gmail.com>
2024-08-08 21:31:55 +0700

    feat: Add cors

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.events.routers import router as events_router
 
 app = FastAPI(
     title="Fitness API",
     docs_url="/api/v1/docs",
     redoc_url="/api/v1/redoc",
+)
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(events_router)
